43cfce74b50f8cb91ede198a97320d5ad8989749
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2024-09-09 13:57:42 -0400

    Set up to refactor code using redux

---
+++ shopping-cart/src/components/CartSlice.jsx
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+
+  }
+
+})
+
+
+
+
+export default CartSlice.reducer;
